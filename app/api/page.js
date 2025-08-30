4fca135d235712f18d35ce40406cd04d99955b73
hs11420 <119895928+hs11420@users.noreply.github.com>
2025-08-30 02:34:42 -0400

    Create page.js

---
+++ app/api/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>AWN Chatbot Backend</h1>
+      <p>API endpoints are running at /api/awn-chat and /api/awn-lead</p>
+    </div>
+  )
+}
